660b5faf2c1eb2bdbb15129259401dd3b0c61474
Albert Li <liwentao90@yahoo.com>
2018-08-15 01:53:51 -0400

    fix and initially support prod process in ts/tsx

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // uncomment only for debuging
-process.env.DEBUG = '*';
+// process.env.DEBUG = '*';
+
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
