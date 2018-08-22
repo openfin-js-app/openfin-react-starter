5a6260d72b1e99205a091f1ac37cc449c1d486da
Albert Li <liwentao90@yahoo.com>
2018-08-22 17:03:42 -0400

    enable log saga for dev profile

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -2,9 +2,20 @@ import { all, takeEvery, select, fork } from 'redux-saga/effects';
 
 import applicationSaga from './application';
 
+function* watchAndLog(){
+    yield takeEvery('*',function* logger(action) {
+        const state = yield select();
+        console.log(`${action.type}`,action,'state after',state);
+    })
+}
+
 export default function* rootSaga(){
 
-    let sagas = [applicationSaga()];
+    let sagas = [applicationSaga(),];
+
+    if(process.env['REACT_APP_LOG_ACTION']=='true'){
+        sagas.unshift(watchAndLog());
+    }
 
     yield all(sagas);
 
