05507cfbe8aa58e3519a6c64c927497458947df4
Albert Li <liwentao90@yahoo.com>
2018-10-22 14:50:56 -0400

    - add test cases to test saga index.ts

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -3,11 +3,13 @@ import { all, takeEvery, select, fork } from 'redux-saga/effects';
 import applicationSaga from './application';
 import configSaga from './config';
 
-function* watchAndLog(){
-    yield takeEvery('*',function* logger(action) {
-        const state = yield select();
-        console.log(`${action.type}`,action,'state after',state);
-    })
+export function* handleLogAllActions(action) {
+    const state = yield select();
+    console.log(`${action.type}`,action,'state after',state);
+}
+
+export function* watchAndLogSaga(){
+    yield takeEvery('*',handleLogAllActions)
 }
 
 export default function* rootSaga(){
@@ -15,7 +17,7 @@ export default function* rootSaga(){
     const sagas = [applicationSaga(),configSaga(),];
 
     if(process.env.REACT_APP_LOG_ACTION === 'true'){
-        sagas.unshift(watchAndLog());
+        sagas.unshift(watchAndLogSaga());
     }
 
     yield all(sagas);
