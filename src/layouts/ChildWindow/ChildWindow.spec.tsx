a88a308732e6e40a3fc0f6d37f8005c7cd6ee6ae
Albert Li <liwentao90@yahoo.com>
2018-10-23 21:54:41 -0400

    - add test cases to ChildWindow layout 2#

--- src/layouts/ChildWindow/ChildWindow.spec.tsx
+++ src/layouts/ChildWindow/ChildWindow.spec.tsx
@@ -111,6 +111,44 @@ describe('ChildWindow layout',()=>{
         expect(currentWindow.getState).toHaveBeenCalled();
     });
 
+    it('could be closed',()=>{
+        let lastWindowState = 'normal';
+        const currentWindow = {
+            setAsForeground:jest.fn(),
+            getState:jest.fn((cb)=>(cb(lastWindowState))),
+            minimize:jest.fn(()=>{
+                lastWindowState = 'minimized'
+            }),
+            restore:jest.fn(()=>{
+                lastWindowState = 'normal'
+            }),
+            maximize:jest.fn(()=>{
+                lastWindowState = 'maximized'
+            }),
+            close:jest.fn(),
+        };
+
+        window.fin={
+            desktop:{
+                Window:{
+                    getCurrent: ()=> currentWindow
+                }
+            }
+        };
+
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/childWindow/view-one']}>
+                <ChildWindow store={store} location={{pathname:'/childWindow/view-one'}}/>
+            </MemoryRouter>
+        );
+        expect(wrapper.find(ViewOne)).toHaveLength(1);
+        expect(wrapper.find(ChildWindow)).toHaveLength(1);
+        expect(wrapper.find(Header)).toHaveLength(1);
+        wrapper.find(Header).props().onClose();
+        expect(currentWindow.close).toHaveBeenCalled();
+        expect(currentWindow.getState).toHaveBeenCalled();
+    });
+
     it('render correctly on maximized window state',()=>{
         let lastWindowState = 'maximized';
         const currentWindow = {
