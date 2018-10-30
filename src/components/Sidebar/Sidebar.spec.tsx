2b1099ce50606d8615cc3581d0ebb76285cc748f
Albert Li <liwentao90@yahoo.com>
2018-10-30 01:12:16 -0400

    - add test cases to Sidebar comp

---
+++ src/components/Sidebar/Sidebar.spec.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import toJson from 'enzyme-to-json';
+import { createShallow, createMount } from '@material-ui/core/test-utils';
+
+import Sidebar from './Sidebar';
+
+describe('Sidebar comp',()=>{
+
+    let shallow;
+    let mount;
+
+    beforeAll(() => {
+        mount = createMount();
+    });
+
+    afterAll(() => {
+        mount.cleanUp();
+    });
+
+
+    beforeEach(() => {
+        shallow = createShallow();
+    });
+
+    it('renders correctly by default',()=>{
+
+        const onClose = jest.fn()
+
+        const wrapper = shallow(<Sidebar
+            open={true}
+            routes={[]}
+            color = {'primary'}
+            image = {undefined}
+        />);
+
+        const component = wrapper.dive();
+        expect(toJson(component)).toMatchSnapshot();
+
+    })
+
+});
