d61f4c1d854080ae9550addce510a3a8079642be
Albert Li <liwentao90@yahoo.com>
2019-01-01 14:23:27 -0500

    - init setup pkg structure 4#

--- scripts/package.js
+++ scripts/package.js
@@ -7,9 +7,12 @@ require('../config/env');
 const os = require('os');
 
 const chalk = require('chalk');
+const fse = require('fs-extra');
 const log = console.log;
+const path = require('path');
 const shell = require('shelljs');
 const { exec } = require('pkg');
+
 
 let platform = os.platform();
 
@@ -26,15 +29,25 @@ if(platform==='darwin'){
 }
 
 const target = `node10-${platform}-${process.env.STANDALONE_TARGET_PLATFORM}`;
-log(chalk.green('TARGET',target));
-log(chalk.green('PWD',shell.pwd()));
 
-shell.mkdir('package');
-shell.cp('.env*','package');
-shell.cp('-R','build','package');
-shell.cp('package.json','package');
+const createPackageFile = async () => {
+    const packageData = await fse.readFile(path.resolve(__dirname,'../package.json'),'utf-8');
+    const {scripts, devDependencies, jest, pkg, ...packageDataOther} = JSON.parse(packageData);
 
-// pkg scripts/server.js --target node10-linux-x64 --output openfin_starter_server
+    const newPackageData = {
+        ...packageDataOther,
+        private:false,
+    };
+
+    const buildPath = path.resolve(__dirname,'../package/package.json');
+
+    await fse.writeFile(buildPath,JSON.stringify(newPackageData,null,2),'utf8');
+    log(chalk.green(`Created package.json in ${buildPath}`));
+
+    return newPackageData;
+}
+
+
 const buildBinary = async (script,name)=>{
     await exec([ script, '--target', target, '--output', name])
 };
@@ -43,5 +56,17 @@ const build = async() => {
     await buildBinary('scripts/server.js',`package/${process.env.STANDALONE_SERVER_NAME}`);
     await buildBinary('scripts/standalone.openfin.js',`package/${process.env.STANDALONE_NAME}`);
 };
+
+
+log(chalk.green('TARGET',target));
+log(chalk.green('PWD',shell.pwd()));
+
+shell.mkdir('package');
+shell.cp('.env','package');
+shell.cp('.env.production','package');
+shell.cp('-R','build','package');
+createPackageFile();
+
+// pkg scripts/server.js --target node10-linux-x64 --output openfin_starter_server
 
 build();
