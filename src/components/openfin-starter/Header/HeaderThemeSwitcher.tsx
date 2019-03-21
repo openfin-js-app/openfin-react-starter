edc51144c7640eb1e1540b5123b9f1378b1a0f7b
Albert Li <liwentao90@yahoo.com>
2019-03-21 15:46:23 -0400

    - initially refactor HeaderThemeSwitcher comp

--- src/components/openfin-starter/Header/HeaderThemeSwitcher.tsx
+++ src/components/openfin-starter/Header/HeaderThemeSwitcher.tsx
@@ -17,36 +17,28 @@ interface IProps extends WithStyles<typeof style>, WithConfigContext {
 
 }
 
-interface IState{
-    [key:number]:any,
-    [key:string]:any,
-}
-
-class HeaderThemeSwitcherComp extends React.Component<IProps, IState>{
-    render(){
-
-        const {
-            classes,
-            configContext:{
-                config,
-                actions:{
-                    onToggleThemeField
-                }
+const HeaderThemeSwitcherComp:React.FunctionComponent<IProps> = (
+    {
+        classes,
+        configContext:{
+            config,
+            actions:{
+                onToggleThemeField
             }
-        } = this.props;
-
-        return(
-            <div
-                className={cx(
-                    classes.switcher,
-                    {
-                        [classes.active]: config.application.theme === MuiTheme.DARK
-                    }
-                )}
-                onClick={onToggleThemeField}
-            />
-        )
+        }
     }
+)=>{
+    return(
+        <div
+            className={cx(
+                classes.switcher,
+                {
+                    [classes.active]: config.application.theme === MuiTheme.DARK
+                }
+            )}
+            onClick={onToggleThemeField}
+        />
+    )
 }
 
 export default withStyles(style)(
