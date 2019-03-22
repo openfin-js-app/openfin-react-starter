cca8ef61c8da4eacec941c038546ac3a2d2c3100
Albert Li <liwentao90@yahoo.com>
2019-03-22 17:10:23 -0400

    - initially refactor App entry comp

--- src/App.tsx
+++ src/App.tsx
@@ -28,149 +28,141 @@ interface IProps{
     [key:string]:any,
 }
 
-interface IState {
-    [key:number]:any,
-    [key:string]:any,
+const buildMuiTheme = (theme:MuiTheme)=>{
+    return createMuiTheme({
+        typography:{
+            useNextVariants:true,
+            fontSize: 8,
+            htmlFontSize: 10,
+        },
+        palette:{
+            type:theme,
+            primary:{
+                main:primaryColor,
+            },
+            secondary:{
+                main:infoColor,
+            },
+            error:{
+                main:dangerColor,
+            },
+        },
+        overrides:{
+            MuiButton:{
+                root:{
+                    color:'white',
+                },
+                contained:{
+                    padding:'0px 5px',
+                    minWidth:'50px',
+                    minHeight:'20px',
+                    color:'white',
+                },
+                containedPrimary:{
+                    color:'white',
+                },
+                containedSecondary:{
+                    color:'white',
+                },
+                sizeSmall:{
+                    padding:'0px 5px',
+                    minWidth:'40px',
+                    minHeight:'16px',
+                },
+                sizeLarge:{
+                    padding:'0px 5px',
+                    minWidth:'60px',
+                    minHeight:'25px',
+                },
+            },
+            MuiChip:{
+                root:{
+                    height:'20px',
+                },
+                avatar:{
+                    width:'20px',
+                    height:'20px',
+                    fontSize:'0.5rem',
+                },
+            },
+            MuiFab:{
+                root:{
+                    padding: '0px 0px',
+                    width:'40px',
+                    height:'40px',
+                }
+            },
+            MuiIconButton:{
+                root:{
+                    padding: '0px 0px',
+                }
+            },
+            MuiListItem:{
+                gutters:{
+                    paddingTop:'2px',
+                    paddingBottom:'2px',
+                }
+            },
+            MuiSnackbarContent:{
+                root:{
+                    color:'white',
+                }
+            },
+            MuiTabs:{
+                root:{
+                    minHeight:'24px',
+                }
+            },
+            MuiTab:{
+                root:{
+                    minHeight:'24px',
+                },
+                labelContainer:{
+                    paddingTop:'0px',
+                    paddingBottom:'0px',
+                }
+            },
+        }
+    })
+};
+
+
+const App:React.FunctionComponent<IProps> = (
+    {
+        config,
+        theme,
+        actions:{
+            handleToggleThemeField, handleUpdateLangField
+        }
+    }
+)=>{
+    return (
+        <React.Fragment>
+            <CssBaseline/>
+            <Router history={hist}>
+                <MuiThemeProvider theme={buildMuiTheme(theme)}>
+                    <GlobalContext
+                        config={config}
+                        onToggleThemeField={handleToggleThemeField}
+                        onUpdateLangField={handleUpdateLangField}
+                    >
+                        <Switch>
+                            {
+                                indexRoutes.map((prop:any,key)=>{
+                                    if (prop.redirect)
+                                        return <Redirect from={prop.path} to={prop.to} key={key}/>;
+                                    return <Route path={prop.path} component={prop.component} key={key}/>;
+
+                                })
+                            }
+                        </Switch>
+                    </GlobalContext>
+                </MuiThemeProvider>
+            </Router>
+        </React.Fragment>
+    );
 }
 
-
-class App extends React.Component<IProps,IState>{
-
-    createMuiTheme = ()=>{
-        const { theme } = this.props;
-        return createMuiTheme({
-            typography:{
-                useNextVariants:true,
-                fontSize: 8,
-                htmlFontSize: 10,
-            },
-            palette:{
-                type:theme,
-                primary:{
-                    main:primaryColor,
-                },
-                secondary:{
-                    main:infoColor,
-                },
-                error:{
-                    main:dangerColor,
-                },
-            },
-            overrides:{
-                MuiButton:{
-                    root:{
-                        color:'white',
-                    },
-                    contained:{
-                        padding:'0px 5px',
-                        minWidth:'50px',
-                        minHeight:'20px',
-                        color:'white',
-                    },
-                    containedPrimary:{
-                        color:'white',
-                    },
-                    containedSecondary:{
-                        color:'white',
-                    },
-                    sizeSmall:{
-                        padding:'0px 5px',
-                        minWidth:'40px',
-                        minHeight:'16px',
-                    },
-                    sizeLarge:{
-                        padding:'0px 5px',
-                        minWidth:'60px',
-                        minHeight:'25px',
-                    },
-                },
-                MuiChip:{
-                    root:{
-                        height:'20px',
-                    },
-                    avatar:{
-                        width:'20px',
-                        height:'20px',
-                        fontSize:'0.5rem',
-                    },
-                },
-                MuiFab:{
-                    root:{
-                        padding: '0px 0px',
-                        width:'40px',
-                        height:'40px',
-                    }
-                },
-                MuiIconButton:{
-                    root:{
-                        padding: '0px 0px',
-                    }
-                },
-                MuiListItem:{
-                    gutters:{
-                        paddingTop:'2px',
-                        paddingBottom:'2px',
-                    }
-                },
-                MuiSnackbarContent:{
-                    root:{
-                        color:'white',
-                    }
-                },
-                MuiTabs:{
-                    root:{
-                        minHeight:'24px',
-                    }
-                },
-                MuiTab:{
-                    root:{
-                        minHeight:'24px',
-                    },
-                    labelContainer:{
-                        paddingTop:'0px',
-                        paddingBottom:'0px',
-                    }
-                },
-            }
-        })
-    }
-
-    render(){
-
-        const {
-            config,
-            actions:{
-                handleToggleThemeField, handleUpdateLangField
-            }
-        } = this.props;
-
-        return (
-            <React.Fragment>
-                <CssBaseline/>
-                    <Router history={hist}>
-                        <MuiThemeProvider theme={this.createMuiTheme()}>
-                            <GlobalContext
-                                config={config}
-                                onToggleThemeField={handleToggleThemeField}
-                                onUpdateLangField={handleUpdateLangField}
-                            >
-                                <Switch>
-                                    {
-                                        indexRoutes.map((prop:any,key)=>{
-                                            if (prop.redirect)
-                                                return <Redirect from={prop.path} to={prop.to} key={key}/>;
-                                            return <Route path={prop.path} component={prop.component} key={key}/>;
-
-                                        })
-                                    }
-                                </Switch>
-                            </GlobalContext>
-                        </MuiThemeProvider>
-                    </Router>
-            </React.Fragment>
-        );
-    }
-}
 
 export default connect(
     (state:IRootState)=>({
